a57fcbff2b403890989dc5bee99c50d8ae77d676
sujan-shrestha <sujanstha406@gmail.com>
2019-06-04 00:58:46 +0545

    Add Splash Screen/Initial Screen

--- app/src/main/java/com/example/roomfinder/Login.java
+++ app/src/main/java/com/example/roomfinder/Login.java
@@ -2,8 +2,11 @@ package com.example.roomfinder;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.EditText;
 
 public class Login extends AppCompatActivity {
+
+    private EditText etEmail, etPassword;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
