0e8fe76480fbe3ae1f436bb3315f5f5927e52822
sujan-shrestha <sujanstha406@gmail.com>
2019-07-05 14:55:48 +0545

    Successful Login From Firebase API

--- app/src/main/java/com/example/roomfinder/Login.java
+++ app/src/main/java/com/example/roomfinder/Login.java
@@ -1,16 +1,123 @@
 package com.example.roomfinder;
 
+import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
 import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class Login extends AppCompatActivity {
 
-    private EditText etEmail, etPassword;
+    private FirebaseAuth auth;
+    private EditText etEmail, etPass;
+    private CheckBox cbRemember;
+    private TextView tvForget;
+    private Button btnLogin,btnRegister;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        //To get firebase auth
+
+        auth = FirebaseAuth.getInstance();
+
+        if (auth.getCurrentUser() != null) {
+            startActivity(new Intent(Login.this, MainActivity.class));
+            finish();
+        }
+
+        etEmail = findViewById(R.id.etEmail);
+        etPass = findViewById(R.id.etPass);
+        cbRemember = findViewById(R.id.cbRemember);
+        tvForget = findViewById(R.id.tvForget);
+        btnLogin = findViewById(R.id.btnLogin);
+        btnRegister = findViewById(R.id.btnRegister);
+
+        auth = FirebaseAuth.getInstance();
+
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent intent = new Intent(Login.this, Register.class);
+                startActivity(intent);
+
+            }
+        });
+
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String txt_email = etEmail.getText().toString();
+
+                String txt_password = etPass.getText().toString();
+
+                if (TextUtils.isEmpty(txt_email) || TextUtils.isEmpty(txt_password)) {
+
+                    Toast.makeText(Login.this, "All fields are required", Toast.LENGTH_SHORT).show();
+
+                } else {
+
+                    login(txt_email, txt_password);
+
+                }
+
+            }
+        });
+
     }
+
+
+    private  void login(final String txt_email, String txt_password){
+
+
+        auth.signInWithEmailAndPassword(txt_email, txt_password)
+                .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+
+                        if (task.isSuccessful()) {
+
+                            Intent intent = new Intent(Login.this, MainActivity.class);
+
+                            intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK);
+
+                            startActivity(intent);
+
+                            finish();
+
+                            Toast.makeText(Login.this, "Login successful", Toast.LENGTH_SHORT).show();
+
+                        } else {
+
+                            {
+
+
+                                Toast.makeText(Login.this, "Email or Password incorrect", Toast.LENGTH_SHORT).show();
+
+                            }
+
+
+                        }
+
+                    }
+                });
+
+
+
+    }
+
 }
